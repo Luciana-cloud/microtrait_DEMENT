8d1666c4713b419bf131ff57debaeda1dccd19ba
Luciana-cloud <lucianagoku@hotmail.com>
2023-07-31 01:47:11 -0700

    I have improved the analysis of functional groups. Now, I need to start with the tradeoffs.

--- new_functional_grouping.R
+++ new_functional_grouping.R
@@ -11,6 +11,12 @@ library(ggplot2)
 library(stats)
 library(corrr)
 library(olsrr)
+library(vegan)
+library(devtools)
+#install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+library(pairwiseAdonis)
+library(EcolUtils)
+library(ggvegan)
 
 # Calling data and preprocessing ####
 
@@ -32,8 +38,9 @@ mat_trait    = mat_ori %>% select(2:191)/gen_size$length
 mat_trait    = as.data.frame(cbind(mat_ori$id,mat_trait))
 mat_trait    = mat_trait %>% mutate(id_2 = seq(from = 1, to = 531, by = 1))
 
-# Aim 1 ####
-# Traits responsible for adaptations under drought in grassland in Loma ####
+# Aim 1 : Traits responsible for adaptations in Loma ####
+
+# DROUGHT GRASSLAND ####
 
 # Ratio of abundances drought grassland ####
 mag_stat.1 = mag_stat %>% mutate(ratio1.1 = log10((mag_stat$Average.1/mag_stat$Average)*
@@ -143,7 +150,7 @@ g1 + scale_fill_manual(values=c("#a6d96a","#c6dbef",
                                 "#6baed6","#41b6c4","#0570b0","#006d2c",
                                 "#a50026","#f46d43","#d73027","#08306b"))
 
-# Traits responsible for adaptations under drought in shrubland in Loma ####
+# DROUGHT SHRUBLAND ####
 
 # Ratio of abundances drought shrubland ####
 mag_stat.2 = mag_stat %>% mutate(ratio1.1 = log10((mag_stat$Average.3/mag_stat$Average.2)*
@@ -250,7 +257,7 @@ g1 + scale_fill_manual(values=c("#a6d96a","#c6dbef",
                                 "#999999","#6baed6","#41b6c4","#006d2c",
                                 "#a50026","#f46d43","#d73027","#08306b"))
 
-# Traits responsible for adaptations under in shrubland in Loma ####
+# SHRUBLAND ####
 
 # Ratio of abundances shrubland ####
 mag_stat.3 = mag_stat %>% mutate(ratio1.1 = log10((mag_stat$Average.2/mag_stat$Average)*
@@ -351,13 +358,312 @@ g1 + scale_fill_manual(values=c("#a6d96a","#c6dbef",
                                 "#999999","#6baed6","#41b6c4","#006d2c",
                                 "#a50026","#f46d43","#d73027","#08306b"))
 
-# Aim 2 ####
+# Aim 2 : Functional groups under different conditions ####
 
-# Functional groups under different conditions
+# DROUGHT GRASSLAND ####
 
 ratio     = as.data.frame((mag_stat$Average.1/mag_stat$Average))
 colnames(ratio) = "ratio"
 test.1    = as.data.frame(cbind(test.1,ratio))
 test.1    = as.data.frame(test.1[c(-163,-164,-263,-299,-387,-396,-399,-425,-479,
                                    -485,-487,-491,-499,-506,-511),])
+trait_tar = scale(test.1[,2:45],center = FALSE)
 
+# with_mean=False to avoid breaking the sparsity structure of the data
+
+set.seed(1)
+distanceg.1  = vegdist(trait_tar, method = "euclidean", binary = FALSE)
+clusterg.1   = hclust(distanceg.1, method="ward")
+nguilds.1    = seq(2, nrow(trait_tar), 2)
+plot(clusterg.1)
+
+# Similiarity within guilds
+my_vec = c()
+
+for(i in nguilds.1) {
+  set.seed(1)
+  v                      = cutree(clusterg.1,k=i)
+  genome2guild           = data.frame(guild = factor(v))
+  rownames(genome2guild) = names(v)
+  adonis_2               = vegan::adonis2(distanceg.1 ~ guild, data = genome2guild, perm = 1)
+  temp                   = adonis_2$R2
+  temp_1                 = temp[1]
+  my_out                 = temp_1
+  my_vec   <- c(my_vec, my_out)   
+}
+mat_r2_1  = as.data.frame(cbind(nguilds.1,my_vec))
+#write.csv(mat_r2_1, file = "mat_r2_1.csv")
+mat_r2_1  = read.csv(file = "mat_r2_1.csv")
+
+my_vec = c()
+
+for(i in nguilds.1[25]) {
+  v                      = cutree(clusterg.1,k=i)
+  genome2guild           = data.frame(guild = factor(v))
+  rownames(genome2guild) = names(v)
+  adonis_2               = vegan::adonis2(distanceg.1 ~ guild, data = genome2guild, perm = 999)
+  temp                   = adonis_2$`Pr(>F)`
+  temp_1                 = temp[1]
+  my_out                 = temp_1
+  my_vec   <- c(my_vec, my_out)   
+}
+mat_p_1  = as.data.frame(cbind(nguilds.1,my_vec))
+write.csv(mat_p_1, file = "mat_p_1.csv")
+
+v                      = cutree(clusterg.1,k=16)
+genome2guild           = data.frame(guild = factor(v))
+rownames(genome2guild) = names(v)
+adonis_1               = pairwiseAdonis::pairwise.adonis(distanceg.1,genome2guild$guild,perm = 999)
+test.clus.1            = adonis.pair(distanceg.1, genome2guild[,"guild"], nper = 1000, 
+                                     corr.method = "fdr")
+
+ggplot(data=mat_r2_1,aes(x=nguilds.1,y=my_vec)) + geom_line() +
+  xlab("# of guilds") + ylab("Similarity within guilds") +
+  theme(text = element_text(size = 16)) + 
+  geom_hline(yintercept=0.45930709, linetype="dashed", color = "red") + 
+  geom_vline(xintercept = 16, linetype="dashed", color = "red")
+
+# Working with 16 guilds
+
+v.1                       = cutree(clusterg.1,k=16) # Clusters
+genome2guild_16           = data.frame(guild = factor(v))
+rownames(genome2guild_16) = names(v.1)
+mat_ori$row_names         = row.names(mat_ori)
+mat_ori.1                 = as.data.frame(mat_ori[c(-163,-164,-263,-299,-387,-396,-399,-425,-479,
+                                                   -485,-487,-491,-499,-506,-511),])
+mat_ori.1                 = as.data.frame(cbind(mat_ori.1,test.1$ratio))
+mat_trait_1a              = as.data.frame(cbind(genome2guild_16,mat_ori.1$id,trait_tar)) 
+mat.1                     = mat_trait_1a %>% group_by(guild) %>% summarize_if(is.numeric, mean, na.rm=TRUE)
+mat.1                     = (as.matrix(mat.1[2:45]))
+a.1                       = as.data.frame(colnames(mat.1))
+
+set.seed(27)
+ordination_1              = metaMDS(trait_tar,autotransform = F,trymax = 100)
+fort.1                    = fortify(ordination_1)
+# write.csv(fort.1, file = "fort.1.csv")
+fort.1  = read.csv("fort.1.csv")
+
+ggplot() + geom_point(data=subset(fort.1,Score=="sites"),
+                      mapping = aes(x=NMDS1,y=NMDS2,color =mat_trait_1a$guild,size = 2),
+                      alpha=0.5) + 
+  geom_segment(data=subset(fort.1,Score=="species"),
+               mapping=aes(x=0,y=0,xend=NMDS1,yend=NMDS2),
+               arrow=arrow(length=unit(0.015,"npc"),
+                           type="closed"),
+               colour="darkgray",
+               linewidth=0.8) + 
+  geom_text(data=subset(fort.1,Score=="species"), # "species"
+            mapping=aes(label=Label,x=NMDS1*1.1,y=NMDS2*1.1)) + 
+  geom_abline(intercept=0,slope=0,linetype="dashed",linewidth=0.8,colour="gray") + 
+  geom_vline(aes(xintercept=0),linetype="dashed",linewidth=0.8,colour="gray") + 
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_blank(),
+        axis.line=element_line(colour="black")) + 
+  annotate("text", x=-1, y=-1, label=paste('Stress =',round(ordination_1$stress,2)))
+
+# DROGUTH SHRUBLAND ####
+
+ratio.1   = as.data.frame((mag_stat$Average.3/mag_stat$Average.2))
+colnames(ratio.1) = "ratio"
+test.2    = as.data.frame(cbind(test.2,ratio.1))
+test.2    = as.data.frame(test.2[c(-164,-166,-186,-283,-311,-321,-343,-488,-47,
+                                   -163,-171,-172,-178,-187,-253,-262,-263,-275,
+                                   -284,-299),])
+trait_tar.2 = scale(test.2[,2:43],center = FALSE)
+# with_mean=False to avoid breaking the sparsity structure of the data
+
+set.seed(1)
+distanceg.2  = vegdist(trait_tar.2, method = "euclidean", binary = FALSE)
+clusterg.2   = hclust(distanceg.2, method="ward")
+nguilds.2    = seq(2, nrow(trait_tar.2), 2)
+plot(clusterg.2)
+
+# Similiarity within guilds
+my_vec = c()
+
+for(i in nguilds.2) {
+  set.seed(1)
+  v                      = cutree(clusterg.2,k=i)
+  genome2guild           = data.frame(guild = factor(v))
+  rownames(genome2guild) = names(v)
+  adonis_2               = vegan::adonis2(distanceg.2 ~ guild, data = genome2guild, perm = 1)
+  temp                   = adonis_2$R2
+  temp_1                 = temp[1]
+  my_out                 = temp_1
+  my_vec   <- c(my_vec, my_out)   
+}
+mat_r2_2  = as.data.frame(cbind(nguilds.2,my_vec))
+# write.csv(mat_r2_2, file = "mat_r2_2.csv")
+mat_r2_2  = read.csv(file = "mat_r2_2.csv")
+
+my_vec = c()
+
+for(i in nguilds.2[25]) {
+  v                      = cutree(clusterg.2,k=i)
+  genome2guild           = data.frame(guild = factor(v))
+  rownames(genome2guild) = names(v)
+  adonis_2               = vegan::adonis2(distanceg.2 ~ guild, data = genome2guild, perm = 999)
+  temp                   = adonis_2$`Pr(>F)`
+  temp_1                 = temp[1]
+  my_out                 = temp_1
+  my_vec   <- c(my_vec, my_out)   
+}
+mat_p_2  = as.data.frame(cbind(nguilds.2,my_vec))
+write.csv(mat_p_2, file = "mat_p_2.csv")
+
+v                      = cutree(clusterg.2,k=16)
+genome2guild           = data.frame(guild = factor(v))
+rownames(genome2guild) = names(v)
+adonis_2               = pairwiseAdonis::pairwise.adonis(distanceg.2,genome2guild$guild,perm = 999)
+test.clus.2            = adonis.pair(distanceg.2, genome2guild[,"guild"], nper = 1000, 
+                                     corr.method = "fdr")
+
+ggplot(data=mat_r2_2,aes(x=nguilds.2,y=my_vec)) + geom_line() +
+  xlab("# of guilds") + ylab("Similarity within guilds") +
+  theme(text = element_text(size = 16)) + 
+  geom_hline(yintercept=0.50630268, linetype="dashed", color = "red") + 
+  geom_vline(xintercept = 16, linetype="dashed", color = "red")
+
+# Working with 16 guilds
+
+v.2                       = cutree(clusterg.2,k=16) # Clusters
+genome2guild_16           = data.frame(guild = factor(v.2))
+rownames(genome2guild_16) = names(v.2)
+mat_ori$row_names         = row.names(mat_ori)
+mat_ori.2                 = as.data.frame(mat_ori[c(-164,-166,-186,-283,-311,-321,-343,-488,-47,
+                                                    -163,-171,-172,-178,-187,-253,-262,-263,-275,
+                                                    -284,-299),])
+mat_ori.2                 = as.data.frame(cbind(mat_ori.2,test.2$ratio))
+mat_trait_2a              = as.data.frame(cbind(genome2guild_16,mat_ori.2$id,trait_tar.2)) 
+mat.2                     = mat_trait_2a %>% group_by(guild) %>% summarize_if(is.numeric, mean, na.rm=TRUE)
+mat.2                     = (as.matrix(mat.2[2:43]))
+a.2                       = as.data.frame(colnames(mat.2))
+
+set.seed(25)
+ordination_2              = metaMDS(trait_tar.2,autotransform = F,trymax = 100)
+fort.2                    = fortify(ordination_2)
+# write.csv(fort.2, file = "fort.2.csv")
+fort.2  = read.csv("fort.2.csv")
+
+ggplot() + geom_point(data=subset(fort.2,Score=="sites"),
+                      mapping = aes(x=NMDS1,y=NMDS2,color =mat_trait_2a$guild,size = 2),
+                      alpha=0.5) + 
+  geom_segment(data=subset(fort.2,Score=="species"),
+               mapping=aes(x=0,y=0,xend=NMDS1,yend=NMDS2),
+               arrow=arrow(length=unit(0.015,"npc"),
+                           type="closed"),
+               colour="darkgray",
+               linewidth=0.8) + 
+  geom_text(data=subset(fort.2,Score==""), # "species"
+            mapping=aes(label=Label,x=NMDS1*1.1,y=NMDS2*1.1)) + 
+  geom_abline(intercept=0,slope=0,linetype="dashed",linewidth=0.8,colour="gray") + 
+  geom_vline(aes(xintercept=0),linetype="dashed",linewidth=0.8,colour="gray") + 
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_blank(),
+        axis.line=element_line(colour="black")) + 
+  annotate("text", x=-1, y=-1, label=paste('Stress =',round(ordination_2$stress,2)))
+
+# SHRUBLAND ####
+
+ratio.2   = as.data.frame((mag_stat$Average.2/mag_stat$Average))
+colnames(ratio.2) = "ratio"
+test.3    = as.data.frame(cbind(test.3,ratio.2))
+test.3    = as.data.frame(test.3[c(-163,-164,-263,-299,-387,-396,-399,-425,-479,
+                                   -485,-487,-491,-499,-506,-511),])
+trait_tar.3 = scale(test.3[,2:34],center = FALSE)
+# with_mean=False to avoid breaking the sparsity structure of the data
+
+set.seed(1)
+distanceg.3  = vegdist(trait_tar.3, method = "euclidean", binary = FALSE)
+clusterg.3   = hclust(distanceg.3, method="ward")
+nguilds.3    = seq(2, nrow(trait_tar.3), 2)
+plot(clusterg.3)
+
+# Similiarity within guilds
+my_vec = c()
+
+for(i in nguilds.3) {
+  set.seed(1)
+  v                      = cutree(clusterg.3,k=i)
+  genome2guild           = data.frame(guild = factor(v))
+  rownames(genome2guild) = names(v)
+  adonis_2               = vegan::adonis2(distanceg.3 ~ guild, data = genome2guild, perm = 1)
+  temp                   = adonis_2$R2
+  temp_1                 = temp[1]
+  my_out                 = temp_1
+  my_vec   <- c(my_vec, my_out)   
+}
+mat_r2_3  = as.data.frame(cbind(nguilds.3,my_vec))
+# write.csv(mat_r2_3, file = "mat_r2_3.csv")
+mat_r2_3  = read.csv(file = "mat_r2_3.csv")
+
+my_vec = c()
+
+for(i in nguilds.3[25]) {
+  v                      = cutree(clusterg.3,k=i)
+  genome2guild           = data.frame(guild = factor(v))
+  rownames(genome2guild) = names(v)
+  adonis_2               = vegan::adonis2(distanceg.3 ~ guild, data = genome2guild, perm = 999)
+  temp                   = adonis_2$`Pr(>F)`
+  temp_1                 = temp[1]
+  my_out                 = temp_1
+  my_vec   <- c(my_vec, my_out)   
+}
+mat_p_3  = as.data.frame(cbind(nguilds.3,my_vec))
+write.csv(mat_p_3, file = "mat_p_3.csv")
+
+v                      = cutree(clusterg.3,k=15)
+genome2guild           = data.frame(guild = factor(v))
+rownames(genome2guild) = names(v)
+adonis_3               = pairwiseAdonis::pairwise.adonis(distanceg.3,genome2guild[,"guild"],perm = 999)
+adonis.pair(distanceg.3, genome2guild[,"guild"], nper = 1000, corr.method = "bonferroni")
+adonis.pair(distanceg.3, genome2guild[,"guild"], nper = 1000, corr.method = "holm")
+test.clus.3            = adonis.pair(distanceg.3, genome2guild[,"guild"], nper = 1000, 
+                                     corr.method = "fdr")
+
+ggplot(data=mat_r2_3,aes(x=nguilds.3,y=my_vec)) + geom_line() +
+  xlab("# of guilds") + ylab("Similarity within guilds") +
+  theme(text = element_text(size = 15)) + 
+  geom_hline(yintercept=0.54575, linetype="dashed", color = "red") + 
+  geom_vline(xintercept = 15, linetype="dashed", color = "red")
+
+# Working with 15 guilds
+
+v.3                       = cutree(clusterg.3,k=15) # Clusters
+genome2guild_16           = data.frame(guild = factor(v.3))
+rownames(genome2guild_16) = names(v.3)
+mat_ori$row_names         = row.names(mat_ori)
+mat_ori.3                 = as.data.frame(mat_ori[c(-163,-164,-263,-299,-387,-396,-399,-425,-479,
+                                                    -485,-487,-491,-499,-506,-511),])
+mat_ori.3                 = as.data.frame(cbind(mat_ori.3,test.3$ratio))
+mat_trait_3a              = as.data.frame(cbind(genome2guild_16,mat_ori.3$id,trait_tar.3)) 
+mat.3                     = mat_trait_3a %>% group_by(guild) %>% summarize_if(is.numeric, mean, na.rm=TRUE)
+mat.3                     = (as.matrix(mat.3[2:34]))
+a.3                       = as.data.frame(colnames(mat.3))
+
+set.seed(27)
+ordination_3              = metaMDS(trait_tar.3,autotransform = F,trymax = 100)
+fort.3                    = fortify(ordination_3)
+# write.csv(fort.1, file = "fort.1.csv")
+fort.3  = read.csv("fort.3.csv")
+
+ggplot() + geom_point(data=subset(fort.3,Score=="sites"),
+                      mapping = aes(x=NMDS1,y=NMDS2,color =mat_trait_3a$guild,size = 2),
+                      alpha=0.5) + 
+  geom_segment(data=subset(fort.3,Score=="species"),
+               mapping=aes(x=0,y=0,xend=NMDS1,yend=NMDS2),
+               arrow=arrow(length=unit(0.015,"npc"),
+                           type="closed"),
+               colour="darkgray",
+               linewidth=0.8) + 
+  geom_text(data=subset(fort.3,Score==""), # "species"
+            mapping=aes(label=Label,x=NMDS1*1.1,y=NMDS2*1.1)) + 
+  geom_abline(intercept=0,slope=0,linetype="dashed",linewidth=0.8,colour="gray") + 
+  geom_vline(aes(xintercept=0),linetype="dashed",linewidth=0.8,colour="gray") + 
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_blank(),
+        axis.line=element_line(colour="black")) + 
+  annotate("text", x=-1, y=-1, label=paste('Stress =',round(ordination_3$stress,2)))
