e4c4f9f1b1db0617fac3467d74fa95e0ce2a7d28
Luciana-cloud <lucianagoku@hotmail.com>
2024-06-21 15:14:39 -0700

    Minor changes

--- test_file.R
+++ test_file.R
@@ -16,34 +16,39 @@ library(EcolUtils)
 # remotes::install_github("gavinsimpson/ggvegan")
 library(ggvegan)
 
-total.mags                   = read.csv("/pub/lucianac/r_work_3/total.mags.csv",dec=".")
+total.mags                   = read.csv("/pub/lucianac/r_work_3/MAG_global.csv",dec=".")
+total.mags                   = read.csv("/pub/lucianac/r_work_3/MAG_LOMA.csv",dec=".")
+total.mags                   = read.csv("/pub/lucianac/r_work_3/MAG_FIRE.csv",dec=".")
+total.mags                   = read.csv("/pub/lucianac/r_work_3/MAG_HARVARD.FOREST.csv",dec=".")
+total.mags                   = read.csv("/pub/lucianac/r_work_3/MAG_EAA2017.PLOT.csv",dec=".")
+total.mags                   = read.csv("/pub/lucianac/r_work_3/MAG_GRE.COASSEMBLY.csv",dec=".")
 
 set.seed(1)
-distance.total  = vegdist(total.mags[3:507], method = "jaccard", binary = TRUE)
+distance.total  = vegdist(total.mags[3:length(total.mags)], method = "jaccard", binary = TRUE)
 cluster.total   = hclust(distance.total, method="ward.D2")
 
-v                      = cutree(cluster.total,k=110)
+v                      = cutree(cluster.total,k=143)
 genome2guild           = data.frame(guild = factor(v))
 rownames(genome2guild) = names(v)
 adonis_1               = pairwiseAdonis::pairwise.adonis(distance.total,genome2guild$guild,
                                                          perm = 999,p.adjust.m='BH')
 adonis_1
 
-v                      = cutree(cluster.total,k=120)
+v                      = cutree(cluster.total,k=150)
 genome2guild           = data.frame(guild = factor(v))
 rownames(genome2guild) = names(v)
 adonis_1               = pairwiseAdonis::pairwise.adonis(distance.total,genome2guild$guild,
                                                          perm = 999,p.adjust.m='BH')
 adonis_1
 
-v                      = cutree(cluster.total,k=130)
+v                      = cutree(cluster.total,k=200)
 genome2guild           = data.frame(guild = factor(v))
 rownames(genome2guild) = names(v)
 adonis_1               = pairwiseAdonis::pairwise.adonis(distance.total,genome2guild$guild,
                                                          perm = 999,p.adjust.m='BH')
 adonis_1
 
-v                      = cutree(cluster.total,k=140)
+v                      = cutree(cluster.total,k=300)
 genome2guild           = data.frame(guild = factor(v))
 rownames(genome2guild) = names(v)
 adonis_1               = pairwiseAdonis::pairwise.adonis(distance.total,genome2guild$guild,
